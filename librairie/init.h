7485209c1604d46e0df96fbcff637e3ee41b264b
florian <florian.derlique.etu@univ-lille.fr>
2021-04-20 12:14:23 +0200

    déplacement du fichier init.h

---
+++ librairie/init.h
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct airports{ 
+	char IATA_CODE;
+	char AIRPORT;
+	char CITY;
+	char STATE;
+	char COUNTRY;
+	char LATITUDE;
+	char LONGITUDE;
+	struct compagnie *pt_comp;
+	struct airports* airports_suiv;
+}
+
+struct IATA_AIRLINE
+{
+	char IATA_CODE;
+	char AIRLINE;
+
+}
+
+struct compagnie
+{
+	char AIRLINE;
+	struct vol *pt_vol;
+	struct compagnie* compagnie_suiv;
+}
+
+struct vol
+{
+
+char MONTH;
+char DAY;
+char WEEKDAY;
+char ORG_AIR;
+char DEST_AIR;
+char SCHED_DEP;
+char DEP_DELAY;
+char AIR_TIME;
+char DIST;
+char SCHED_ARR;
+char ARR_DELAY;
+char DIVERTED;
+char CANCELLED;
+struct vol* vol_suiv;
+
+}
