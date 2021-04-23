f5264cb9535158f5c7566d267bff6c8d5bf8d6c8
Corentin Gielen <corentin.gielen.etu@univ-lille.fr>
2021-04-23 16:31:50 +0200

    modification des types

--- librairie/init.h
+++ librairie/init.h
@@ -2,48 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct airports{ 
-	char IATA_CODE;
-	char AIRPORT;
-	char CITY;
-	char STATE;
-	char COUNTRY;
-	char LATITUDE;
-	char LONGITUDE;
-	struct compagnie *pt_comp;
+struct airports {
+	char IATA_CODE[];
+	char AIRPORT[];
+	char CITY[];
+	char STATE[];
+	char COUNTRY[];
+	float LATITUDE[];
+	float LONGITUDE[];
+	struct compagnie* pt_comp;
 	struct airports* airports_suiv;
 }
 
 struct IATA_AIRLINE
 {
-	char IATA_CODE;
-	char AIRLINE;
+	char IATA_CODE[];
+	char AIRLINE[];
 	struct IATA_AIRLINE* iata_suiv;
 }
 
 struct compagnie
 {
-	char AIRLINE;
-	struct vol *pt_vol;
+	char AIRLINE[];
+	struct vol* pt_vol;
 	struct compagnie* compagnie_suiv;
 }
 
 struct vol
 {
 
-char MONTH;
-char DAY;
-char WEEKDAY;
-char ORG_AIR;
-char DEST_AIR;
-char SCHED_DEP;
-char DEP_DELAY;
-char AIR_TIME;
-char DIST;
-char SCHED_ARR;
-char ARR_DELAY;
-char DIVERTED;
-char CANCELLED;
-struct vol* vol_suiv;
+	int MONTH;
+	int DAY;
+	int WEEKDAY;
+	char ORG_AIR[];
+	char DEST_AIR[];
+	int SCHED_DEP[];
+	float DEP_DELAY[];
+	float AIR_TIME[];
+	int DIST[];
+	int SCHED_ARR[];
+	float ARR_DELAY[];
+	short DIVERTED[];
+	short CANCELLED[];
+	struct vol* vol_suiv;
 
 }
