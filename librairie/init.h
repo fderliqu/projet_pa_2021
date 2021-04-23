9d41ad000ea828e4d77b1c41615dfa78461d6a2c
Corentin Gielen <corentin.gielen.etu@univ-lille.fr>
2021-04-23 16:57:55 +0200

    modification des types

--- librairie/init.h
+++ librairie/init.h
@@ -1,49 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define SIZE_acronym 3
+#define SIZE_city 30
+#define SIZE_airport 100
+#define SIZE_airline 50
+#define SIZE_airline_acro 2
 
 struct airports {
-	char IATA_CODE[];
-	char AIRPORT[];
-	char CITY[];
-	char STATE[];
-	char COUNTRY[];
-	float LATITUDE[];
-	float LONGITUDE[];
+	char IATA_CODE[SIZE_acronym];
+	char AIRPORT[SIZE_airport];
+	char CITY[SIZE_city];
+	char STATE[SIZE_city];
+	char COUNTRY[SIZE_acronym];
+	float LATITUDE;
+	float LONGITUDE;
 	struct compagnie* pt_comp;
 	struct airports* airports_suiv;
-}
+};
 
-struct IATA_AIRLINE
-{
-	char IATA_CODE[];
-	char AIRLINE[];
+struct IATA_AIRLINE {
+	char IATA_CODE[SIZE_acronym];
+	char AIRLINE[SIZE_airline];
 	struct IATA_AIRLINE* iata_suiv;
-}
+};
 
-struct compagnie
-{
-	char AIRLINE[];
+struct compagnie {
+	char AIRLINE[SIZE_airline_acro];
 	struct vol* pt_vol;
 	struct compagnie* compagnie_suiv;
-}
+};
 
-struct vol
-{
+struct vol {
 
 	int MONTH;
 	int DAY;
 	int WEEKDAY;
-	char ORG_AIR[];
-	char DEST_AIR[];
-	int SCHED_DEP[];
-	float DEP_DELAY[];
-	float AIR_TIME[];
-	int DIST[];
-	int SCHED_ARR[];
-	float ARR_DELAY[];
-	short DIVERTED[];
-	short CANCELLED[];
+	char ORG_AIR[SIZE_acronym];
+	char DEST_AIR[SIZE_acronym];
+	int SCHED_DEP;
+	float DEP_DELAY;
+	float AIR_TIME;
+	int DIST;
+	int SCHED_ARR;
+	float ARR_DELAY;
+	short DIVERTED;
+	short CANCELLED;
 	struct vol* vol_suiv;
 
-}
+};
