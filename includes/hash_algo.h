f0031b6c4f1b3725a7d706e0096fe38723284a8e
Florian Derlique <florian.derlique.etu@univ-lille.fr>
2021-05-01 20:16:09 +0200

    modification hash_algo.h et déplacement dans le bon fichier

---
+++ includes/hash_algo.h
@@ -0,0 +1,15 @@
+//Variables
+#include"init.h"
+#define MAX_LETTER 26
+#define A_ascii 65
+
+//Fonction donnant l'indice de la table de hash
+int get_hash_index_airport(char *);
+
+void init_ht_airport(struct cellule_airport*)
+
+void init_ht_comp(struct cellule_compagnie*)
+
+void init_ht_date(struct cellule_airport*)
+
+void init_ht_namecomp(struct cellule_airport*)
