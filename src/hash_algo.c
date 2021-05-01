920e70a5556db0a415f78630f1b0733c7e3d6161
Florian Derlique <florian.derlique.etu@univ-lille.fr>
2021-05-01 20:38:34 +0200

    ajout des nouvelles fonctions

--- src/hash_algo.c
+++ src/hash_algo.c
@@ -1,5 +1,5 @@
-#include"../librairie/init.h"
-#include"hash_algo.h"
+#include"../includes/init.h"
+#include"../includes/hash_algo.h"
 
 int my_pow(int a,int b)
 {
@@ -10,7 +10,27 @@ int my_pow(int a,int b)
 int get_hash_index_airport(char iata_code[SIZE_acronym])
 {
     return 
-    iata_code[2]-A_ascii + 
+    (iata_code[2]-A_ascii + 
     (iata_code[1]-A_ascii)*(MAX_LETTER) + 
-    (iata_code[0]-A_ascii)*my_pow(MAX_LETTER,2);
+    (iata_code[0]-A_ascii)*my_pow(MAX_LETTER,2))%max_Hairport;
+}
+
+void init_ht_airport(struct cellule_airport* Ht_comp[max_Hairport])
+{
+    for(int i=0; i<max_Hairport; i++)Ht_comp[i] = NULL;
+}
+
+void init_ht_comp(struct cellule_compagnie* Ht_comp[max_Hcomp])
+{
+    for(int i=0; i<max_Hcomp; i++)Ht_comp[i] = NULL;
+}
+
+void init_ht_date(struct cellule_airport* Ht_comp[max_Hdate])
+{
+    for(int i=0; i<max_Hdate; i++)Ht_comp[i] = NULL;
+}
+
+void init_ht_namecomp(struct cellule_airport* Ht_comp[max_Hnamecomp])
+{
+    for(int i=0; i<max_Hnamecomp; i++)Ht_comp[i] = NULL;
 }
