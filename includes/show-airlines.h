e9b7a455bc4da0893132cba6212678fc16d16ff8
Florian Derlique <florian.derlique.etu@univ-lille.fr>
2021-05-29 16:26:18 +0200

    [ADD] show-airlines function 100% works, we will group all function later

---
+++ includes/show-airlines.h
@@ -0,0 +1,1 @@
+void show_airlines(struct cellule_airport**, struct cellule_compagnieDATA**, char*);
--- src/show-airlines.c
+++ src/show-airlines.c
@@ -1,54 +1,30 @@
-void show_airlines(char IATA_CODE_c[SIZE_airport], struct cellule_airport Htable_airport[max_Hairport], struct IATA_AIRLINE Htable_name_compagnie[max_Hcomp])
+#include "../includes/hash_algo.h"
+#include "../includes/cell_function.h"
+#include "../includes/charge_fichier.h"
+#include "../includes/show-airport.h"
+
+void show_airlines(struct cellule_airport* main_HT[max_Hairport], struct cellule_compagnieDATA* data_HT[max_Hdatacomp], char AIRPORT[SIZE_acronym])
 {
-	int Hcode, cpt_airline, Hcode_name_airline;
-	Hcode = 4;//fonction hcode_airport
-	char* IATA_comp;
 	struct cellule_airport* Buffairport;
-	struct cellule_compagnie* Buffcomp;
-	struct IATA_AIRLINE* Buffnamecomp;
-
-	//recherche dans la table de hachage de l'aeroport .
-	Buffairport = Htable_airport[Hcode];
-	//recherche dans la liste chainer des airport 
-	printf("recherche des compagnie associer a l'aeroport en cours :");
-	while (Buffairport != NULL)
+    struct cellule_compagnie* Buffairline;
+    struct cellule_compagnieDATA* Buffdata;
+	int index = get_hash_index_airport(AIRPORT,max_Hairport);
+	Buffairport = recherche_cellHT_airport(main_HT[index],AIRPORT);
+	if(Buffairport->is_empty)
 	{
-		// si on a trouver l'aeroport rechercher
-		if (strcmp(IATA_CODE_c, (Buffairport->airport.IATA_CODE)) == 0)
-		{
-			//parcours de la Htable des compagnie
-			for (cpt_airline = 0; cpt_airline <= max_Hcomp; cpt_airline++)
-			{
-				Buffcomp = Buffairport->pt_Htable_compagnie[cpt_airline];
-				//parcorus dans la liste chainer des compagnies
-
-				while (Buffcomp != NULL)
-				{
-					IATA_comp = Buffcomp->IATA_CODE;
-					Hcode_name_airline = 5; //fonction hcode_name_airline 
-
-					//parcours de la liste chainer des nom des compagnie
-
-					Buffnamecomp = Htable_name_compagnie[Hcode_name_airline];
-					while (strcmp(Buffnamecomp->IATA_CODE, IATA_comp))
-					{
-						Buffnamecomp = Buffnamecomp->compagnie_suiv;
-					}
-
-					//affichage du code + nom
-					printf("%s, %s \n", IATA_comp, Buffnamecomp->IATA_CODE);
-					Buffcomp = Buffcomp->compagnie_suiv;
-				}
-			}
-			return;//arret du while car bonne airport
-		}
-		else
-		{
-			//recherche du suivant 
-			Buffairport = Buffairport->airport_suiv;
-		}
-
+		printf("No company operates flights at this airport\n");
+		return;
 	}
-	printf("l'aeroport n'existe pas fin du programme.");
-
+	for(int i=0;i<max_Hcomp;i++)
+	{
+		Buffairline = Buffairport->pt_Htable_compagnie[i];
+		while(Buffairline!=NULL)
+		{
+			index = get_hash_index_airline(Buffairline->IATA_CODE,max_Hdatacomp);
+			Buffdata = data_HT[index];
+			printf("%s,%s\n",Buffdata->IATA_CODE,Buffdata->AIRLINE);
+			Buffairline = Buffairline->compagnie_suiv;
+		}
+	}
 }
+	
