a774a217330d926716c9e09bff669d1b88ba1933
Corentin Gielen <corentin.gielen.etu@univ-lille.fr>
2021-05-30 19:20:58 +0200

    [FIX] resolution des problemes de semgentation du a la modification de la structure de donner et a une erreur

--- src/changed-flights.c
+++ src/changed-flights.c
@@ -1,56 +1,55 @@
 
-void changed_flights (struct cellule_airport *Htable_airport[max_Hairport], char date[5])
+#include "../includes/cell_function.h"
+#include "../includes/charge_fichier.h"
+#include "../includes/hash_algo.h"
+
+
+void changed_flights (struct cellule_airport *Htable_airport[max_Hairport], int jour, int mois)
 {
 
-    int cpt_airport, cpt_airline,jours, mois;
-    char charjours[2];
-    char charmois[2];
-
-    // recuperation jour et mois
-    charjours[0] = date[3];
-    charjours[1] = date[2];
-    charmois[0]  = date[0];
-    charmois[1]  = date[1];
-    jours= atoi(charjours);
-    mois= atoi(charmois);
- 
-    struct cellule_airport * Buffairport;
+    int                       cpt_airport, cpt_airline;
+    struct cellule_airport *  Buffairport;
     struct cellule_compagnie *Buffcomp;
     struct cellule_vol_date * Buffvol;
 
 
     printf ("Voici les voles annuler de cet aeroport: \n");
     // parcours des airport
-    for (cpt_airport = 0; cpt_airport <= max_Hairport; cpt_airport++)
+    for (cpt_airport = 0; cpt_airport < max_Hairport; cpt_airport++)
     {
         // parcours de la liste chainer des airports
         Buffairport = Htable_airport[cpt_airport];
         while (Buffairport != NULL)
         {
-            // parcours de la Htable des compagnie
-            for (cpt_airline = 0; cpt_airline <= max_Hcomp; cpt_airline++)
+
+            if (Buffairport->is_empty != 1)
             {
-                Buffcomp = Buffairport->pt_Htable_compagnie[cpt_airline];
-                // parcours de la liste chainer des compagnie
-                while (Buffcomp != NULL)
+
+                // parcours de la Htable des compagnie
+                for (cpt_airline = 0; cpt_airline < max_Hcomp; cpt_airline++)
                 {
-                    // parcours des vols
-                        Buffvol   = Buffcomp->pt_Htable_date[mois];
+                    Buffcomp = Buffairport->pt_Htable_compagnie[cpt_airline];
+                    // parcours de la liste chainer des compagnie
+                    while (Buffcomp != NULL)
+                    {
+                        // parcours des vols
+                        Buffvol = Buffcomp->pt_Htable_date[mois - 1];
                         while (Buffvol != NULL)
                         {
-                            if (Buffvol->vol.DAY==jours && Buffvol->vol.CANCELLED == 1)
+                            if (Buffvol->vol.DAY == jour && Buffvol->vol.CANCELLED == 1)
                             {
-                                printf ("%d %d %d %s %s ", Buffvol->vol.DAY, Buffvol->vol.MONTH,
-                                        Buffvol->vol.WEEKDAY, Buffvol->vol.ORG_AIR, Buffvol->vol.DEST_AIR);
+                                printf ("%d %d %d %s %s %s ", Buffvol->vol.DAY, Buffvol->vol.MONTH,
+                                        Buffvol->vol.WEEKDAY, Buffcomp->IATA_CODE,
+                                        Buffvol->vol.ORG_AIR, Buffvol->vol.DEST_AIR);
                                 printf ("%d %d %d ", Buffvol->vol.SCHED_DEP, Buffvol->vol.DIST,
                                         Buffvol->vol.SCHED_ARR);
-                                printf (" %d %d \n", Buffvol->vol.DIVERTED, Buffvol->vol.CANCELLED);
-
+                                printf (" %hd %hd \n", Buffvol->vol.DIVERTED, Buffvol->vol.CANCELLED);
                             }
                             Buffvol = Buffvol->vol_suiv;
                         }
-                    
-                    Buffcomp = Buffcomp->compagnie_suiv;
+
+                        Buffcomp = Buffcomp->compagnie_suiv;
+                    }
                 }
             }
             Buffairport = Buffairport->airport_suiv;
