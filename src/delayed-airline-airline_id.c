3c44f5d7160dde01815008f1e7acd68aaaf6f8da
Corentin Gielen <corentin.gielen.etu@univ-lille.fr>
2021-05-31 00:58:17 +0200

    [UPDATE][FIX] fix des erreurs de segmentation et passage a une moyenne des delayed uniquement positif

--- src/delayed-airline-airline_id.c
+++ src/delayed-airline-airline_id.c
@@ -1,46 +1,60 @@
-
-void show_most_delayed_flights (struct cellule_airport *Htable_airport[max_Hairport],char IATA_CODE[SIZE_airline_acro])
+void delayed_airline (struct cellule_airport *Htable_airport[max_Hairport], char IATA_CODE[SIZE_airline_acro])
 {
-    int cpt_airport, cpt_date;
-    float totalretard, nbtotal;
+    int                       cpt_airport, cpt_date, indexcomp, cpt;
+    float                     totalretard, nbtotal;
     struct cellule_airport *  Buffairport;
     struct cellule_compagnie *Buffcomp;
     struct cellule_vol_date * Buffvol;
     totalretard = 0;
-    nbtotal = 0;
-    Hcode = get_hash_index_airline (IATA_CODE, 1);
+    nbtotal     = 0;
+    indexcomp   = get_hash_index_airline (IATA_CODE, max_Hcomp);
+    cpt         = 0;
 
 
-    printf ("Voici les 5 vols qui on subis le plus longs retard a l'arriv : \n");
+    printf ("Voici la moyenne de retard de la compagnie %s : \n", IATA_CODE);
     // parcours des airport
-    for (cpt_airport = 0; cpt_airport <= max_Hairport; cpt_airport++)
+    for (cpt_airport = 0; cpt_airport < max_Hairport; cpt_airport++)
     {
         // parcours de la liste chainer des airports
         Buffairport = Htable_airport[cpt_airport];
         while (Buffairport != NULL)
         {
-            // parcours de la liste chainer des compagnies
-            Buffcomp = Buffairport->pt_Htable_compagnie[Hcode];
-            while (Buffcomp->IATA_CODE != IATA_CODE || Buffcomp != NULL)
+            if (Buffairport->is_empty != 1)
             {
-                Buffcomp = Buffcomp->compagnie_suiv;
-            }
-            for (cpt_date = 0; cpt_date <= max_Hdate; cpt_date++)
-            {
-                Buffvol = Buffcomp->pt_Htable_date[cpt_date];
-                while (Buffvol != NULL)
+                // parcours de la liste chainer des compagnies
+                Buffcomp = Buffairport->pt_Htable_compagnie[indexcomp];
+                while (Buffcomp != NULL)
                 {
-                    if (Buffvol->vol.CANCELLED != 1)
+                    if (strcmp (Buffcomp->IATA_CODE, IATA_CODE) == 0)
                     {
-                        nbtotal += 1;
-                        totalretard += Buffvol->vol.ARR_DELAY;
+                        for (cpt_date = 0; cpt_date < max_Hdate; cpt_date++)
+                        {
+                            Buffvol = Buffcomp->pt_Htable_date[cpt_date];
+                            while (Buffvol != NULL)
+                            {
+                                if (Buffvol->vol.CANCELLED != 1 && Buffvol->vol.DIVERTED != 1)
+                                {
+                                    cpt++;
+                                    // printf("AIr delay %f \n ", Buffvol->vol.ARR_DELAY);
+                                    if (Buffvol->vol.ARR_DELAY > 0)
+                                    {
+                                        // printf("AIr retenue %f \n ", Buffvol->vol.ARR_DELAY);
+                                        nbtotal += 1;
+                                        totalretard += Buffvol->vol.ARR_DELAY;
+                                    }
+                                }
+                                Buffvol = Buffvol->vol_suiv;
+                            }
+                        }
                     }
-                    Buffvol = Buffvol->vol_suiv;
+                    Buffcomp = Buffcomp->compagnie_suiv;
                 }
             }
 
             Buffairport = Buffairport->airport_suiv;
         }
     }
-    printf ("la compagnie %s a en moyenne %f", IATA_CODE, totalretard / nbtotal);
+    printf ("la compagnie %s a en moyenne %f \n", IATA_CODE, totalretard / nbtotal);
+    // printf("nbtotal %f \n", nbtotal);
+    // printf("cpt %d\n", cpt);
 }
