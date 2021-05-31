6624cf55cacd41c4966f5b7c853bed10eb650028
Florian Derlique <florian.derlique.etu@univ-lille.fr>
2021-05-31 18:10:58 +0200

    [UPDATE] file finished

--- includes/show-flights.h
+++ includes/show-flights.h
@@ -1,14 +1,6 @@
 
-//Return : 
-//  show-flights <main_HT> <AIRPORT> <M> <J> [<time>] [limit=<xx>] <mask>: 
-//  affiche les vols qui partent de l'aéroport à la date, 
-//  avec optionnellement une heure de début, et limité à xx vols
-//Parameters : 
-//  <main_HT>=table de hash des données des vols
-//  <AIRPORT>=aéroport de départ
-//  <J>=nombre du jour
-//  <M>=nombre du mois
-//  [<time>] = temps début départ (option) 
-//  [limit=<xx>] = limite d'affichage (option)
-//  <mask> masque pour parametre optionnel
-void show_flights(struct cellule_airport**, char*, int, int, int, int, char);
+#define nb_arg_show_flights 2
+#define nb_optionnal_arg_show_flights 2
+
+//Fct init show_flights
+void init_show_flights(struct line_arguments, struct cellule_airport**);
