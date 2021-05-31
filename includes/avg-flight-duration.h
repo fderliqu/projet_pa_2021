f68f678f73956b54f6fdf2b47d4ad9b05972bd0e
Florian Derlique <florian.derlique.etu@univ-lille.fr>
2021-05-31 18:20:33 +0200

    [UPDATE] file finished

--- includes/avg-flight-duration.h
+++ includes/avg-flight-duration.h
@@ -1,1 +1,5 @@
-void avg_flight_duration(struct cellule_airport**, char*, char*);
+#define nb_arg_avg_flight_duration 2
+
+/*struct line_arguments : liste des arguments
+struct cellule_airport** : table données vol + aéroport*/
+void init_avg_flight_duration(struct line_arguments, struct cellule_airport**);
